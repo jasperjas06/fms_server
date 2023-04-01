54424d2c28f7ec196c97e45ad4e9ac21983ebc98
jasperjas06 <jas131059@gmail.com>
2023-04-01 11:13:13 +0530

    added updates

--- config/config.js
+++ config/config.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 const config=async()=>{
    try {
-  
-   await mongoose.connect('mongodb://localhost:27017/kumardeptManage')
+   await mongoose.connect('mongodb://localhost:27017/FMS_RN_Jasper')
 //   await mongoose.connect("mongodb+srv://kumar:kumar@cluster0.mp19l.mongodb.net/?retryWrites=true&w=majority")
-    console.log("DBconected");
-    
+    console.log("DBconected");  
    } catch (error) {
     console.log("error",error);
    }
